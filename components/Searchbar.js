260ea7f98b1978a9b35a843ef5077b8666f0308c
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-11-16 20:52:37 +0530

    Search Bar

---
+++ components/Searchbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Searchbar() {
+  return (
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  );
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
+import Searchbar from "../components/Searchbar";
 
 export default function Home() {
   return (
-    <SafeAreaView style={{ backgroundColor: "#eee" }}>
-      <View>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
+        <Searchbar />
       </View>
     </SafeAreaView>
   );
