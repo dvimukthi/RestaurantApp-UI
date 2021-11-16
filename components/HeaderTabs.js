946b13b89a589a80f9c2fa98beec3fc4ffffa477
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-11-16 16:55:53 +0530

    Header Buttons

---
+++ components/HeaderTabs.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+export default function HeaderTabs() {
+  return (
+    <View>
+      <HeaderButton text="Delivery" />
+      <HeaderButton text="Pickup" />
+    </View>
+  );
+}
+
+const HeaderButton = (props) => (
+  <View>
+    <TouchableOpacity>
+      <Text>{props.text}</Text>
+    </TouchableOpacity>
+  </View>
+);
