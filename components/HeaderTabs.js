764b02478f536ebb749799b4e73403b455119ba2
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-11-16 19:31:34 +0530

    btn color

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
 export default function HeaderTabs() {
+  const [activeTab, setActiveTab] = useState("Delivery");
   return (
-    <View>
-      <HeaderButton text="Delivery" />
-      <HeaderButton text="Pickup" />
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <HeaderButton
+        text="Delivery"
+        btnColor="black"
+        textColor="white"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        text="Pickup"
+        btnColor="white"
+        textColor="black"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
     </View>
   );
 }
 
 const HeaderButton = (props) => (
-  <View>
-    <TouchableOpacity>
-      <Text>{props.text}</Text>
-    </TouchableOpacity>
-  </View>
+  <TouchableOpacity
+    style={{
+      backgroundColor: props.btnColor,
+      paddingVertical: 6,
+      paddingHorizontal: 16,
+      borderRadius: 30,
+    }}
+    onPress={() => props.setActiveTab(props.text)}
+  >
+    <Text style={{ color: props.textColor, fontSize: 15, fontWeight: "900" }}>
+      {props.text}
+    </Text>
+  </TouchableOpacity>
 );
