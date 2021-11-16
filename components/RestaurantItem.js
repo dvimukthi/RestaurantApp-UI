552ec1b30ce16c341e9c4645322e52e1c89cdd73
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-11-17 02:30:55 +0530

    RestaurantImage

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 export default function RestaurantItem() {
   return (
     <View>
-      <Text>Restaurant Item</Text>
+      <RestaurantImage />
     </View>
   );
 }
+
+const RestaurantImage = () => (
+  <Image
+    source={{
+      uri: "https://www.esquiresg.com/wp-content/uploads/2018/05/best-restaurants-world-main-esquire-singapore-1280x720.jpg",
+    }}
+    style={{ width: "100%", height: 180 }}
+  />
+);
