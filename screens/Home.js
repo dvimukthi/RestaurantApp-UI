66933e8cbaaf45a2b052b0b0c97ef0f97fdb2648
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-11-17 02:18:15 +0530

    RestaurantItem

--- screens/Home.js
+++ screens/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
+import RestaurantItem from "../components/RestaurantItem";
 import Searchbar from "../components/Searchbar";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
+        <RestaurantItem />
       </ScrollView>
     </SafeAreaView>
   );
