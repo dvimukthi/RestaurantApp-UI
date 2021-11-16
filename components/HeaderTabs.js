0fa9b36a60263ee3a608421e4d293a5e4c3fb5a9
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-11-16 19:54:38 +0530

    btn color changing

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -26,14 +26,20 @@ export default function HeaderTabs() {
 const HeaderButton = (props) => (
   <TouchableOpacity
     style={{
-      backgroundColor: props.btnColor,
+      backgroundColor: props.activeTab == props.text ? "black" : "white",
       paddingVertical: 6,
       paddingHorizontal: 16,
       borderRadius: 30,
     }}
     onPress={() => props.setActiveTab(props.text)}
   >
-    <Text style={{ color: props.textColor, fontSize: 15, fontWeight: "900" }}>
+    <Text
+      style={{
+        color: props.activeTab == props.text ? "white" : "black",
+        fontSize: 15,
+        fontWeight: "900",
+      }}
+    >
       {props.text}
     </Text>
   </TouchableOpacity>
